10cab081c1299bc2037b4eff726b572a16493a64
erastus munyao kyalo <erastusmunya2022@gmail.com>
2024-01-15 07:00:50 +0300

    improved the create account screen

--- lib/Authentication/Screens/CreateAccount.dart
+++ lib/Authentication/Screens/CreateAccount.dart
@@ -18,111 +18,131 @@ class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ShowLinkedInImage(),
-                const Spacer(),
-                BigText("LINKEDIN", FontWeight.bold, Colors.blue),
-                const Spacer(),
-                BigText("Create   Account", FontWeight.bold, Colors.black),
-                const Spacer(),
-                textFormField(
-                  FullNameController,
-                  TextInputType.text,
-                  const Icon(Icons.person),
-                  null,
-                  false,
-                  "Enter FullName",
-                  "FullName",
-                ),
-                const Spacer(),
-                textFormField(
-                  EmailController,
-                  TextInputType.emailAddress,
-                  const Icon(Icons.email),
-                  null,
-                  false,
-                  "Enter Email",
-                  "Email",
-                ),
-                const Spacer(),
-                textFormField(
-                  PassWordController,
-                  TextInputType.visiblePassword,
-                  const Icon(Icons.key),
-                  const Icon(Icons.visibility_off),
-                  true,
-                  "Enter Password",
-                  "Password",
-                ),
-                const Spacer(),
-                textFormField(
-                  confirmPasswordController,
-                  TextInputType.visiblePassword,
-                  const Icon(Icons.key),
-                  const Icon(Icons.visibility_off),
-                  true,
-                  "please confirm password",
-                  "Confirm password",
-                ),
-                const Spacer(),
-                ShowButton(
-                  Colors.blue,
-                      () {},
-                  "Sign Up",
-                  BorderSide.none,
-                  FontWeight.bold,
-                ),
-                const Spacer(),
-                Row(
-                  children: [
-                    SmallText("Already Have An Account?", Colors.black87),
-                    const SizedBox(width: 5,),
-                    TextButton(
-                      onPressed: () {},
-                      child: SmallText("Log In", Colors.blue),
+           child:  Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ShowLinkedInImage(),
+              BigText("LINKEDIN", FontWeight.bold, Colors.blue),
+              showHeight(),
+              BigText("Create   Account", FontWeight.bold, Colors.black),
+              showHeight(),
+              textFormField(
+                FullNameController,
+                TextInputType.text,
+                const Icon(Icons.person),
+                null,
+                false,
+                "Enter FullName",
+                "FullName",
+              ),
+              showHeight(),
+              textFormField(
+                EmailController,
+                TextInputType.emailAddress,
+                const Icon(Icons.email),
+                null,
+                false,
+                "Enter Email",
+                "Email",
+              ),
+              showHeight(),
+              textFormField(
+                PassWordController,
+                TextInputType.visiblePassword,
+                const Icon(Icons.key),
+                const Icon(Icons.visibility_off),
+                true,
+                "Enter Password",
+                "Password",
+              ),
+              showHeight(),
+              textFormField(
+                confirmPasswordController,
+                TextInputType.visiblePassword,
+                const Icon(Icons.key),
+                const Icon(Icons.visibility_off),
+                true,
+                "please confirm password",
+                "Confirm password",
+              ),
+              showHeight(),
+              showHeight(),
+              ShowButton(
+                Colors.blue,
+                    () {},
+                "Sign Up",
+                BorderSide.none,
+                FontWeight.bold,
+              ),
+              showHeight(),
+              Row(
+                children: [
+                  SmallText("Already Have An Account?", Colors.black87),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: SmallText("Log In", Colors.blue),
+                  ),
+                ],
+              ),
+              showHeight(),
+              showHeight(),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black.withOpacity(0.3),
+                      thickness: 1,
                     ),
-                  ],
-                ),
-                const Spacer(),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black.withOpacity(0.3),
-                        thickness: 1,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: BigText("OR", FontWeight.normal, Colors.black87),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black.withOpacity(0.3),
+                      thickness: 1,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: BigText("OR", FontWeight.normal, Colors.black87),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black.withOpacity(0.3),
-                        thickness: 1,
-                      ),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                ShowElevatedButton(
-                  Colors.white,
-                      () {},
-                  "Sign Up With Google",
-                  const BorderSide(color: Colors.black87),
-                  FontWeight.normal,
-                  "images/google.png",
-                ),
-                const Spacer(),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              showHeight(),
+              ShowElevatedButton(
+                Colors.white,
+                    () {},
+                "Sign Up With Google",
+                const BorderSide(color: Colors.black87),
+                FontWeight.normal,
+                "images/google.png",
+              ),
+              showHeight(),
+              ShowElevatedButton(
+                Colors.white,
+                    () {},
+                "Sign Up With Apple",
+                const BorderSide(color: Colors.black87),
+                FontWeight.normal,
+                "images/apple.png",
+              ),
+              showHeight(),
+              ShowElevatedButton(
+                Colors.white,
+                    () {},
+                "Sign Up With Facebook",
+                const BorderSide(color: Colors.black87),
+                FontWeight.normal,
+                "images/facebook.png",
+              ),
+            ],
           ),
-      ),
+          ),
+        ),
     );
   }
 }
